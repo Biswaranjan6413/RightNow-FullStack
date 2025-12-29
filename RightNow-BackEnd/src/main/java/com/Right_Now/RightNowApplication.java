1b3aab93f098f59fc08ec2d0f18d2c01344b3bbc
Biswa ranjan patra <124027045+Biswaranjan6413@users.noreply.github.com>
2025-12-29 11:29:21 +0530

    first commit front end and back end

---
+++ RightNow-BackEnd/src/main/java/com/Right_Now/RightNowApplication.java
@@ -0,0 +1,13 @@
+package com.Right_Now;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RightNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RightNowApplication.class, args);
+	}
+
+}
---
+++ RightNow-BackEnd/src/test/java/com/Right_Now/RightNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Right_Now;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RightNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
